7d9edeb7caf315464ab4229a7a565717f530f349
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2020-09-23 15:34:36 +0200

    Initial steps
    getting url

---
+++ priceTracker.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    res = request.get("https://www.amazon.es/1home-Ajustable-Escritorio-Pantallas-computadora/dp/B01MR397OH/ref=sr_1_1_sspa?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=soporte+monitor&qid=1600789853&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSFBFSVlHV1UyQjhQJmVuY3J5cHRlZElkPUEwODQ3MzQyMVFZMDJVNE41QkhSWiZlbmNyeXB0ZWRBZElkPUEwMTEwMDU1QUNKT04wRVdDT0RZJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==")
+    conn.request("GET", "/")
+    result = conn.getresponse()
+    contents = result.read()
+   
+    print(res)
+
+
+
+if __name__ == "__main__":
+    main()
